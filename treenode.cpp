f02e29fc167b08428ee710d498789816c5383754
richiebailey74 <57724969+richiebailey74@users.noreply.github.com>
2020-11-29 14:45:08 -0500

    Update treenode.cpp

--- treenode.cpp
+++ treenode.cpp
@@ -1,5 +1,7 @@
 #include "treenode.h"
 
+
+//red black
 treenode::treenode(int input) {
 	date = input;
 	left = NULL;
@@ -14,6 +16,26 @@ void treenode::operator = (const treenode& node) {
 	right = node.right;
 	parent = node.parent;
 	color = node.color;
+	for (unsigned int i = 0; i < node.incidentsContainer.size(); i++) {
+		incidentsContainer.push_back(node.incidentsContainer[i]);
+	}
+}
+
+
+
+//avl
+treeNode::treeNode(int input) {
+	date = input;
+	left = NULL;
+	right = NULL;
+	height = 1;
+}
+
+void treeNode::operator = (const treeNode& node) {
+	date = node.date;
+	left = node.left;
+	right = node.right;
+	height = node.height;
 	for (unsigned int i = 0; i < node.incidentsContainer.size(); i++) {
 		incidentsContainer.push_back(node.incidentsContainer[i]);
 	}
