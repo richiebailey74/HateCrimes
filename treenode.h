8613b29449cb68a6c4399449a33bd3b0e2fe1997
richiebailey74 <57724969+richiebailey74@users.noreply.github.com>
2020-11-29 14:44:33 -0500

    Update treenode.h

--- treenode.h
+++ treenode.h
@@ -6,6 +6,7 @@
 #include "Incident.h"
 using namespace std;
 
+//red black node
 class treenode {
 public:
 	int date;
@@ -17,6 +18,23 @@ public:
 
 	treenode(int input);
 	void operator = (const treenode& node);
+
+};
+
+
+//avl node
+class treeNode {
+public:
+	int date;
+	int height; //equivalent in balancing functionality to RB color boolean
+	vector<Incident*> incidentsContainer;
+	treeNode* left;
+	treeNode* right;
+	//does not need a parent node for effective functionality
+
+	treeNode(int input);
+	void operator = (const treeNode& node);
+
 
 };
 
