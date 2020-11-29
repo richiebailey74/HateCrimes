94aea24b93638fad3953caaca7a4916a08168731
richiebailey74 <57724969+richiebailey74@users.noreply.github.com>
2020-11-29 13:02:00 -0500

    Update treenode.h

--- treenode.h
+++ treenode.h
@@ -3,13 +3,14 @@
 #pragma once
 #include <iostream>
 #include <vector>
+#include "Incident.h"
 using namespace std;
 
 class treenode {
 public:
 	int date;
 	bool color; //true is black, false is red
-	vector<int> incidentsContainer; //will have incident objects instead of int
+	vector<Incident> incidentsContainer; //will have incident objects instead of int
 	treenode* left;
 	treenode* right;
 	treenode* parent; //doubly linked to make way easier implementation
