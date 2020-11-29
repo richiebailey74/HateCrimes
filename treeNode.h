e8fa8e140e2d3dba4ac18d9b9a7f42103f5a2504
richiebailey74 <57724969+richiebailey74@users.noreply.github.com>
2020-11-29 13:00:24 -0500

    Update treeNode.h

--- treeNode.h
+++ treeNode.h
@@ -4,13 +4,14 @@
 #pragma once
 #include <iostream>
 #include <vector>
+#inlcude "Incident.h"
 using namespace std;
 
 class treeNode {
 public:
 	int date;
 	int height; //equivalent in balancing functionality to RB color boolean
-	vector<int> incidentsContainer;
+	vector<Incident> incidentsContainer;
 	treeNode* left;
 	treeNode* right;
 	//does not need a parent node for effective functionality
