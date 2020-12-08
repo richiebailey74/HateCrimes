af1587bc8799a1728d00aee8af71e2c5bb154409
richiebailey74 <57724969+richiebailey74@users.noreply.github.com>
2020-12-07 23:13:25 -0500

    Update Incident.h

--- Incident.h
+++ Incident.h
@@ -1,12 +1,14 @@
 #pragma once
 using namespace std;
+
 class Incident {
 public:
     string state;
     int date;
 
-    //functions
+    //default constructir initializes the incident to have essentially an invalid date of 1 (doesn't even adhere to format)
     Incident() : state(""), date(-1) {}
+    //constructor takes in input values and assigns them to the member variables
     Incident(string state, int date) {this->state = state; this->date = date;}
 
 };
