c001b79360d47e0433613985a4c8de2638fc99d3
richiebailey74 <57724969+richiebailey74@users.noreply.github.com>
2020-12-07 22:45:31 -0500

    Update AVLtree.h

--- AVLtree.h
+++ AVLtree.h
@@ -8,23 +8,24 @@ using namespace std;
 
 class AVLtree {
 private:
+	//member variables
 	treeNode* root;
 	int treeSize;
 
+	//rotation functions
 	treeNode* leftRotation(treeNode*& currentnode);
 	treeNode* rightRotation(treeNode*& currentnode);
 	treeNode* leftRightRotation(treeNode*& currentnode);
 	treeNode* rightLeftRotation(treeNode*& currentnode);
 
-	//helper functions
+	//helper functions - don't need public access
 	void updateHeight(treeNode* node);
 	int height(treeNode* node);
 	treeNode* treeInsert(treeNode* currentnode, treeNode* insertnode);
 	void inOrderRecursive(treeNode* currentnnode);
 	void levelOrderRecursive(treeNode* currentnode);
 	int BF(treeNode* node);
-	void traversalIncidentDate(treeNode* currentnode, Incident* incidentToAdd, bool& searchSuccess); //might need to add a date
-	//add away functions to abstract away functionality later
+	void traversalIncidentDate(treeNode* currentnode, Incident* incidentToAdd, bool& searchSuccess);
 	void searchYearIncidentDateRecursive(treeNode* currentnode, int& incidentSum, int year);
 	void deleteTreeRecursive(treeNode* &currNode);
 	void deleteTree();
